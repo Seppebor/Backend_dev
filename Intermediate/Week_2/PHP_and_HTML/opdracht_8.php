d8ea50cfb21910b3f9000114474a4ab1ab2a78b9
Saestion <seppe.borgmans@student.kdg.be>
2020-12-26 01:12:47 +0100

    Week 2: PHP&HTML, Http Methods, Sessions&Cookies

---
+++ Intermediate/Week_2/PHP_and_HTML/opdracht_8.php
@@ -0,0 +1,65 @@
+<?php 
+$output;
+$cars = array (
+	array("Mercedes","rood","M-klasse"),
+	array("Mercedes","wit","r-klasse"),
+	array("Mercedes","rood","r-klasse"),
+	array("BMW","wit","X1"),
+	array("BMW","wit","X3"),
+  	array("BMW","geel","X1"),
+	array("Audi","rood","A1"),
+	array("Audi","zwart","A3"),
+	array("Audi","zwart","A1"),
+);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Opdracht 8</title>
+</head>
+<body>
+	
+<?php if($input = $_GET['brand']): ?>
+	<?php for ($row = 0; $row < 9; $row++): ?> 	
+		<?php if($input == $cars[$row][0]): ?>	
+	<p>
+			<?php for ($col = 0; $col < 3; $col++): ?> 
+				<span><?php echo $cars[$row][$col]; 
+?></span>
+			<?php endfor; ?>
+	</p>
+		<?php endif; ?>
+	<?php endfor; ?>
+
+	
+	
+<?php elseif($input = $_GET['color']): ?>
+	<?php for ($row = 0; $row < 9; $row++): ?>
+		<?php if($input == $cars[$row][1]): ?>
+	<p>
+			<?php for ($col = 0; $col < 3; $col++): ?> 
+				<span><?php echo $cars[$row][$col]; 
+?></span>
+			<?php endfor; ?>
+	</p>
+		<?php endif; ?>
+	<?php endfor; ?>
+
+	
+	
+<?php elseif($input = $_GET['model']): ?>
+	<?php for ($row = 0; $row < 9; $row++): ?> 
+		<?php if($input == $cars[$row][2]): ?>
+	<p>
+			<?php for ($col = 0; $col < 3; $col++): ?> 
+				<span><?php echo $cars[$row][$col]; 
+?></span>
+			<?php endfor; ?>
+	</p>
+		<?php endif; ?>
+	<?php endfor; ?>
+<?php endif; ?>
+</body>
+</html>
